6611a3b56b0fe449c4442b7a322366f0d847bc05
Suryansh Yadav <expertsy23@gmail.com>
2021-06-19 11:32:30 +0530

    Added Role Api

--- src/controller/role.js
+++ src/controller/role.js
@@ -12,3 +12,33 @@ exports.createInitialRole = async (req, res) => {
     res.status(400).json({ error: "not saved" });
   }
 };
+
+exports.createRole = async (req, res) => {
+  const {name,features,enable}=req.body
+  const role = await new Role({
+    name,features,enable
+  }).save();
+  if (role) {
+    res.status(201).json({ role });
+  } else {
+    res.status(400).json({ error: "Unable to create" });
+  }
+};
+
+exports.updateRole=async(req,res)=>{
+  Role.findByIdAndUpdate(req.params.id,req.body, (err,Role)=>{
+    if (err) {
+      return res.status(500).send({error: "Problem with Updating the Role"})
+    };
+    res.send({success: "Updation successfull"});
+  })
+}  
+
+exports.deleteRole=async(req,res)=>{
+  Role.findByIdAndUpdate(req.params.id,req.body, (err,Role)=>{
+    if (err) {
+      return res.status(500).send({error: "Problem with Updating the Role"})
+    };
+    res.send({success: "Deletion successfull"});
+  })
+}  
